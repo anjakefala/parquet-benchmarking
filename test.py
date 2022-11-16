435cc31c5c0850f695be4445da01a1b65e8466ff
anjakefala <anja@voltrondata.com>
2022-11-15 20:56:31 -0800

    Increase number of rows of integer array

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import pyarrow.parquet as pq
 import numpy as np
 import pandas as pd
 
-df = pd.DataFrame(np.random.randint(0,1000000,size=(1000000, 4)), columns=list('ABCD'))
+df = pd.DataFrame(np.random.randint(0,1000000000,size=(100000000, 4)), columns=list('ABCD'))
 
 table = pa.Table.from_pandas(df)
 
