52d1101c9f85a9a7316bea6c9f1d1c469e10a9bc
anjakefala <anja@voltrondata.com>
2022-11-16 16:36:50 -0800

    Add string generation

--- test.py
+++ test.py
@@ -3,9 +3,23 @@ import pyarrow.parquet as pq
 
 import numpy as np
 import pandas as pd
+from pandas.util.testing import rands
 
-df = pd.DataFrame(np.random.randint(0,1000000000,size=(100000000, 4)), columns=list('ABCD'))
+def generate_strings(nrows, nunique, string_length=10):
+    unique_values = [rands(string_length) for i in range(nunique)]
+    values = unique_values * (nrows // nunique)
+    return values
 
-table = pa.Table.from_pandas(df)
+def test_integers():
+    df = pd.DataFrame(np.random.randint(0,1000000000,size=(100000000, 4)), columns=list('ABCD'))
 
-pq.write_table(table, 'integers.parquet')
+    table = pa.Table.from_pandas(df)
+
+    pq.write_table(table, 'integers.parquet')
+
+def test_many_short_strings():
+    df = pd.DataFrame(generate_strings(1000000, 1000), columns=['str'])
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, 'strings.parquet')
+
+test_many_short_strings()
